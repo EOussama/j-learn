e8ada54cb1f5aaeb8f2263f150e9c6f97695535c
EOussama <oussama_gtacj@live.fr>
2018-05-06 02:30:20 +0100

    More changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+import json
 
 app = Flask(__name__)
 
@@ -7,4 +9,12 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    print('...')
+    if os.path.exists('data/info.json'):
+
+        info = json.load(open('data/info.json', encoding="utf8"))
+        print('Info', info)
+    else:
+        open('data/info.json', 'w')
+
     app.run(debug=True)
